b2a9586a636596baa03f96a9a771fa9fc6728efd
Miguel <miguellara@igeekstudios.com>
2017-06-05 14:45:58 -0800

    Got Jquery, waypoints interactivity working on home page.

--- ClientAngular/src/app/home/home.component.ts
+++ ClientAngular/src/app/home/home.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./css/animate.css','./css/font-awesome.css','./css/Grid.css','./css/latofonts.css','./css/main.css','./css/Site.css','./css/queries.css']
 })
 export class HomeComponent implements OnInit {
+  pets: any = [
+    {name: 'Molly', tag: '123456', age: 3, gender: 'female', type: 'dog', weight: 3, color: 'Brown/Black', breed: 'Yorkie',imagePath: 'http://www.lillehei-yorkies.co.za/wp-content/uploads/2013/06/MILA-BOY-SMALL-3-600x500.jpg'},
+    {name: 'Molly', tag: '123456', age: 3, gender: 'female', type: 'dog', weight: 3, color: 'Brown/Black', breed: 'Yorkie',imagePath: 'http://www.lillehei-yorkies.co.za/wp-content/uploads/2013/06/MILA-BOY-SMALL-3-600x500.jpg'},
+    {name: 'Molly', tag: '123456', age: 3, gender: 'female', type: 'dog', weight: 3, color: 'Brown/Black', breed: 'Yorkie',imagePath: 'http://www.lillehei-yorkies.co.za/wp-content/uploads/2013/06/MILA-BOY-SMALL-3-600x500.jpg'},
+    {name: 'Molly', tag: '123456', age: 3, gender: 'female', type: 'dog', weight: 3, color: 'Brown/Black', breed: 'Yorkie',imagePath: 'http://www.lillehei-yorkies.co.za/wp-content/uploads/2013/06/MILA-BOY-SMALL-3-600x500.jpg'}
+  ];
 
   constructor() { }
 
