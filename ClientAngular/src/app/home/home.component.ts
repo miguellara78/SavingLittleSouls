2ab1b816efe8b669cf990200416e8bede970cdff
Miguel <miguellara@igeekstudios.com>
2017-06-08 14:58:15 -0800

    Centralized font-awesome, set bootstrap 4 for layout.

--- ClientAngular/src/app/home/home.component.ts
+++ ClientAngular/src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import {PetsService} from "../services/pets.service";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./css/animate.css','./css/font-awesome.css','./css/Grid.css','./css/latofonts.css','./css/main.css','./css/Site.css','./css/queries.css']
+  styleUrls: ['./css/animate.css','./css/Grid.css','./css/latofonts.css','./css/main.css','./css/Site.css','./css/queries.css']
 })
 export class HomeComponent implements OnInit {
   pets: any = [];
